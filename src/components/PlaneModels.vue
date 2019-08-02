f9236eec88433208f5139faa8f403bdf5c9924fa
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2019-08-02 17:54:39 +0200

    Add a progress bar

--- src/components/PlaneModels.vue
+++ src/components/PlaneModels.vue
@@ -1,52 +1,57 @@
 <template>
-  <div class="container columns">
-    <div class="column">
-      <form method="POST" enctype="multipart/form-data" class="form box">
-        <label class="label" for="description">Description</label>
-        <input
-          class="input"
-          type="text"
-          id="description"
-          name="description"
-          v-model="newModel.desc"
-        />
-        <label class="label" for="id">ID</label>
-        <input
-          class="input"
-          type="text"
-          id="id"
-          name="id"
-          v-model="newModel.id"
-        />
-        <div>
-          <input type="file" name="image" id="uploadImage" />
-        </div>
-        <div class="wrapper">
-          <div
-            class="entry"
-            v-for="(price, index) in newModel.price"
-            :key="index"
-          >
-            <label class="label" :for="index">{{ scales[index] }}</label>
-            <input
-              class="input"
-              type="number"
-              :name="scales[index]"
-              :id="index"
-              v-model.number="newModel.price[index]"
-            />
+  <div class="container">
+    <progress class="progress" :value="percentage" max="100" v-if="uploading"
+      >{{ percentage }}%</progress
+    >
+    <div class="columns">
+      <div class="column">
+        <form method="POST" enctype="multipart/form-data" class="form box">
+          <label class="label" for="description">Description</label>
+          <input
+            class="input"
+            type="text"
+            id="description"
+            name="description"
+            v-model="newModel.desc"
+          />
+          <label class="label" for="id">ID</label>
+          <input
+            class="input"
+            type="text"
+            id="id"
+            name="id"
+            v-model="newModel.id"
+          />
+          <div>
+            <input type="file" name="image" id="uploadImage" />
           </div>
-        </div>
-        <input type="button" v-on:click="upload" value="Send" />
-      </form>
-    </div>
-    <div class="column">
-      <Model
-        v-for="model in models$"
-        :key="model['.key']"
-        :model="model"
-        :category="category"
-      />
+          <div class="wrapper">
+            <div
+              class="entry"
+              v-for="(price, index) in newModel.price"
+              :key="index"
+            >
+              <label class="label" :for="index">{{ scales[index] }}</label>
+              <input
+                class="input"
+                type="number"
+                :name="scales[index]"
+                :id="index"
+                v-model.number="newModel.price[index]"
+              />
+            </div>
+          </div>
+          <input type="button" v-on:click="upload" value="Send" />
+        </form>
+      </div>
+      <div class="column">
+        <Model
+          v-for="model in models$"
+          :key="model['.key']"
+          :model="model"
+          :category="category"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -55,7 +60,7 @@
 import Model from "./Model.vue";
 import { db, storage } from "../firestore";
 import { collectionData } from "rxfire/firestore";
-import { startWith, tap } from "rxjs/operators";
+import { startWith, tap, finalize } from "rxjs/operators";
 import { percentage } from "rxfire/storage";
 export default {
   name: "Models",
@@ -67,7 +72,9 @@ export default {
         price: [0, 0, 0, 0]
       },
       scales: ["1:144", "1:72", "1:48", "1:32"],
-      category: "lotnictwo"
+      category: "lotnictwo",
+      uploading: false,
+      percentage: 0
     };
   },
   components: {
@@ -75,20 +82,24 @@ export default {
   },
   methods: {
     upload() {
-      let storageRef = storage.ref(`${this.$data.chosenCategory}`);
+      let storageRef = storage.ref(`${this.$data.category}`);
       let image = document.querySelector("#uploadImage").files[0];
       let res = image.name.match(/\.[0-9a-z]+$/i);
       let imageRef = storageRef.child(`${this.$data.newModel.id}${res[0]}`);
-      let databaseRef = db.collection(`${this.$data.chosenCategory}`);
-      let uploadTask = imageRef
-        .put(image)
-        .then(snapshot => {
-          console.log(snapshot);
-        })
-        .catch(err => console.log(err));
-      percentage(uploadTask).subscribe(action => {
-        console.log(action.progress, action.snapshot);
-      });
+      let databaseRef = db.collection(`${this.$data.category}`);
+      let uploadTask = imageRef.put(image);
+      this.$data.uploading = true;
+      percentage(uploadTask)
+        .pipe(
+          finalize(() => {
+            console.log("Finished.");
+            this.$data.uploading = false;
+            this.$data.percentage = 0;
+          })
+        )
+        .subscribe(action => {
+          this.$data.percentage = action.progress;
+        });
       databaseRef
         .add(this.$data.newModel)
         .then(data => console.log(data))
--- src/components/TrainModels.vue
+++ src/components/TrainModels.vue
@@ -1,52 +1,57 @@
 <template>
-  <div class="container columns">
-    <div class="column">
-      <form method="POST" enctype="multipart/form-data" class="form box">
-        <label class="label" for="description">Description</label>
-        <input
-          class="input"
-          type="text"
-          id="description"
-          name="description"
-          v-model="newModel.desc"
-        />
-        <label class="label" for="id">ID</label>
-        <input
-          class="input"
-          type="text"
-          id="id"
-          name="id"
-          v-model="newModel.id"
-        />
-        <div>
-          <input type="file" name="image" id="uploadImage" />
-        </div>
-        <div class="wrapper">
-          <div
-            class="entry"
-            v-for="(price, index) in newModel.price"
-            :key="index"
-          >
-            <label class="label" :for="index">{{ scales[index] }}</label>
-            <input
-              class="input"
-              type="number"
-              :name="scales[index]"
-              :id="index"
-              v-model.number="newModel.price[index]"
-            />
+  <div class="container">
+    <progress class="progress" :value="percentage" max="100" v-if="uploading"
+      >{{ percentage }}%</progress
+    >
+    <div class="columns">
+      <div class="column">
+        <form method="POST" enctype="multipart/form-data" class="form box">
+          <label class="label" for="description">Description</label>
+          <input
+            class="input"
+            type="text"
+            id="description"
+            name="description"
+            v-model="newModel.desc"
+          />
+          <label class="label" for="id">ID</label>
+          <input
+            class="input"
+            type="text"
+            id="id"
+            name="id"
+            v-model="newModel.id"
+          />
+          <div>
+            <input type="file" name="image" id="uploadImage" />
           </div>
-        </div>
-        <input type="button" v-on:click="upload" value="Send" />
-      </form>
-    </div>
-    <div class="column">
-      <Model
-        v-for="model in models$"
-        :key="model['.key']"
-        :model="model"
-        :category="category"
-      />
+          <div class="wrapper">
+            <div
+              class="entry"
+              v-for="(price, index) in newModel.price"
+              :key="index"
+            >
+              <label class="label" :for="index">{{ scales[index] }}</label>
+              <input
+                class="input"
+                type="number"
+                :name="scales[index]"
+                :id="index"
+                v-model.number="newModel.price[index]"
+              />
+            </div>
+          </div>
+          <input type="button" v-on:click="upload" value="Send" />
+        </form>
+      </div>
+      <div class="column">
+        <Model
+          v-for="model in models$"
+          :key="model['.key']"
+          :model="model"
+          :category="category"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -55,7 +60,7 @@
 import Model from "./Model.vue";
 import { db, storage } from "../firestore";
 import { collectionData } from "rxfire/firestore";
-import { startWith, tap } from "rxjs/operators";
+import { startWith, tap, finalize } from "rxjs/operators";
 import { percentage } from "rxfire/storage";
 export default {
   name: "Models",
@@ -63,7 +68,7 @@ export default {
     return {
       newModel: {
         desc: "",
-        id: "MD000",
+        id: "MDK000",
         price: [0, 0, 0]
       },
       scales: ["TT", "H0", "1:35"],
@@ -75,34 +80,38 @@ export default {
   },
   methods: {
     upload() {
-      let storageRef = storage.ref("kolej");
+      let storageRef = storage.ref(`${this.$data.category}`);
       let image = document.querySelector("#uploadImage").files[0];
       let res = image.name.match(/\.[0-9a-z]+$/i);
       let imageRef = storageRef.child(`${this.$data.newModel.id}${res[0]}`);
-      let databaseRef = db.collection("kolej");
-      let uploadTask = imageRef
-        .put(image)
-        .then(snapshot => {
-          console.log(snapshot);
-        })
-        .catch(err => console.log(err));
-      percentage(uploadTask).subscribe(action => {
-        console.log(action.progress, action.snapshot);
-      });
+      let databaseRef = db.collection(`${this.$data.category}`);
+      let uploadTask = imageRef.put(image);
+      this.$data.uploading = true;
+      percentage(uploadTask)
+        .pipe(
+          finalize(() => {
+            console.log("Finished.");
+            this.$data.uploading = false;
+            this.$data.percentage = 0;
+          })
+        )
+        .subscribe(action => {
+          this.$data.percentage = action.progress;
+        });
       databaseRef
         .add(this.$data.newModel)
         .then(data => console.log(data))
         .catch(err => console.log(err));
       this.$data.newModel = {
         desc: "",
-        id: "MD000",
+        id: "MDK000",
         price: [0, 0, 0]
       };
       image = null;
     }
   },
   subscriptions() {
-    const query = db.collection("kolej").orderBy("id", "desc");
+    const query = db.collection(this.$data.category).orderBy("id", "desc");
     return {
       models$: collectionData(query, ".key").pipe(
         startWith([]),
